e2c0c712fe0290b0b086bf2f28d9e1bde0853fa4
Martin D. Hermansen <martin.hermansen@hotmail.com>
2020-09-25 10:29:08 +0200

    La til vendingMachine i øving 5.

---
+++ ovinger/oving5/vending-machine-modification-mdherman/main.cpp
@@ -0,0 +1,45 @@
+#include <cstdlib>              // Exit codes for main.
+#include <iostream>             // std::cout
+
+#include <soda.h>               // Include Soda class header (makes class declaration visible).
+#include <vendingmachine.h>     // Include VendingMachine clase header (makes class declaration visible).
+
+
+// This is a C++ style comment. Compiler will ignore all comments.
+
+
+// The C++ program starts execution here.
+int main()
+{
+    // Print a message at startup.
+
+    // Create vendingMachine object. Constructor will be called with no parameters.
+    VendingMachine vendingMachine;
+    // Print inventory using public printInventory member function (method).
+    vendingMachine.printInventory();
+
+
+    // Create a cola object. Constructor will be called with given parameters.
+    Soda cola("Colabrus", 34, 3);
+    Soda solo("Solobrus", 30, 3);
+    Soda water("Vann", 21, 3);
+
+    // We have a variable named cola with Soda as its data type.
+
+    // Call the VendingMachine addType function with cola variable as parameter.
+    vendingMachine.addType(cola);
+    vendingMachine.addType(solo);
+    vendingMachine.addType(water);
+    // Print inventory using public printInventory member function (method).
+    vendingMachine.printInventory();
+
+
+
+    // Print a message just before exiting.
+    std::cout << "* * *" << std::endl;
+    std::cout << "Vending machine shutting down ... BYE!" << std::endl << std::endl;
+
+
+    // Signal "no error occured" to the world outside our small application.
+    return EXIT_SUCCESS; // Could also return 0 but this is the most correct way...
+}
